446d8c71d622be7328fbfd669bb74af6a73b0788
scanlanjames <scanlan.james@gmail.com>
2016-07-16 00:14:46 -0400

    set tests to validate required API

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -14,26 +14,20 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Tag.count') do
 	entity_tags=@tag.entity_tags
 	entity_tags=[entity_tags] if !entity_tags.nil? && entity_tags.is_a?(String)
-      post tags_url, params: { tag: { entity_id: @tag.entity_id, entity_tags: entity_tags, entity_type: @tag.entity_type } }, as: :json
+      post "/tag", params: { tag: { entity_id: @tag.entity_id, entity_tags: entity_tags, entity_type: @tag.entity_type } }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show tag" do
-    get tag_url(@tag), as: :json
+    get "/tags/#{@tag.id}", as: :json
     assert_response :success
   end
 
-# patch is not supported
-  #test "should update tag" do
- #   patch tag_url(@tag), params: { tag: { entity_id: @tag.entity_id, entity_tags: @tag.entity_tags, entity_type: @tag.entity_type } }, as: :json
- #   assert_response 200
- # end
-
   test "should destroy tag" do
     assert_difference('Tag.count', -1) do
-      delete tag_url(@tag), as: :json
+      delete "/tags/#{@tag.id}", as: :json
     end
 
     assert_response 204
